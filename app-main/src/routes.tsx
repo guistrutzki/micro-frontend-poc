1237e337d8404e16ead456dc0bfa9b2b674a5e91
Guilherme Strutzki <guilhermestrutzk@hotmail.com>
2021-05-24 09:57:04 -0300

    Add app routes

---
+++ app-main/src/routes.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom'
+
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+
+const Routes: FC = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Dashboard}/>
+      <Route exact path="/signin" component={Login}/>
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
--- app-root/src/gs-root-config.ts
+++ app-root/src/gs-root-config.ts
@@ -1,15 +1,6 @@
 import { registerApplication, start } from "single-spa";
 
 registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
-
-registerApplication({
   name: "@gs/app-main",
   app: () => System.import("@gs/app-main"),
   activeWhen: ["/"]
